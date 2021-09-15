c5a6720d4e242dfeafadf16ba1c863d66fd4cf96
Camilla Grevy <camillagrevy@gmail.com>
2021-09-15 23:21:59 +0200

    Add list of commands chatbot can provide

--- backend/backend.js
+++ backend/backend.js
@@ -14,22 +14,30 @@ app.use(
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('{"Hello": "world!"}')
-})
 
 app.post('/', (req, res) => {
-  const wordArray = req.body.message.split(' ');
-  var f;
+  const wordArray = req.body.message.toLowerCase().split(' ');
+  let f;
   fs.readFile('conf.json', function(err, data) {
     f = JSON.parse(data);
-    var topicArray = [];
+    let topicArray = [];
     wordArray.forEach(word => {
       topicArray = topicArray.concat(f.filter(topic => topic.tag.some(t => t == word )) )   
     });
 
     if(topicArray.length < 1){
-      topicArray.push(f.find(topic => topic.tag == 'XIDontUnderstand') )
+      const commands = f.map(topic => topic.tag[0]).filter(command => command[0] !== 'X');
+      let message = f.find(topic => topic.tag == 'XIDontUnderstand');
+      for(c in commands){
+        if(c == 0){
+          message.message += ' ' + commands[c];
+        }else{
+          message.message += ', ' + commands[c];
+        }
+      }
+
+      message.message += '.';
+      topicArray.push(message)
     }else {
     // TODO: This is a hack for adding the "can I help you with anything else" message. This should be done in another way..
     topicArray.push(f.find(topic => topic.tag == 'XGoodbye'))
